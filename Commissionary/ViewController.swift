c930357974d907d9bd721354a870e4de5cba4271
Danniel Galindo <dgalindosanchez5113@stu.d214.org>
2021-09-29 08:57:18 -0700

    Initial Commit

---
+++ Commissionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Commissionary
+//
+//  Created by Danniel Galindo Sanchez on 07/07/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
